ab466d2521fd4014d3e039abc4021ed0ae9269fa
Zukina <tibor.zukina@cumnsee.com>
2021-04-17 00:18:26 +0200

    Fetching HTML page from MongoDB added

--- pagefetch.py
+++ pagefetch.py
@@ -1,6 +1,30 @@
 import urllib.request
+import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+mongoConnection = None
+webpagesDatabaseName = None
+webpagesCollectionName = None
+
+class ConnectionNotSetException(Exception):
+    pass
 
 def fetchRawHTML(url):
     webURL = urllib.request.urlopen(url)
     html = webURL.read()
+    return html
+
+def setMongoConnection(connectionString, databaseName, collectionName):
+    global mongoConnection, webpagesDatabaseName, webpagesCollectionName
+    mongoConnection = MongoClient(connectionString)
+    webpagesDatabaseName = databaseName
+    webpagesCollectionName = collectionName
+	
+def fetchMongoHTML(pageId):
+    if mongoConnection is None:
+        raise ConnectionNotSetException("MongoDB connection not set")
+    webpagesDatabase = mongoConnection[webpagesDatabaseName]
+    webpagesCollection = webpagesDatabase[webpagesCollectionName]
+    html = webpagesCollection.find_one({"_id": ObjectId(pageId)})['page'];
     return html
