cd453f71d760a0c63bdd1b6473fe267bd5be51ef
Zukina <tibor.zukina@cumnsee.com>
2021-05-25 00:10:38 +0200

    Database load added

---
+++ database_load.py
@@ -0,0 +1,8 @@
+import pagefetch
+
+connectionString = open('connection_string', 'r').readline()
+databaseName = 'websecradar'
+collectionName = 'crawled_data_pages_v0'
+
+pagefetch.setMongoConnection(connectionString, databaseName, collectionName)
+pagefetch.fetchMongoHTMLDatabase(10000,10000)
